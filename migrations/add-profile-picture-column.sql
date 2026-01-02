257aca43046b6ba596e919ec3d34b5908487d762
chrrispin <chrrispin@gmail.com>
2026-01-02 20:45:23 +0200

    fix: Correct profile picture migration SQL syntax

--- migrations/add-profile-picture-column.sql
+++ migrations/add-profile-picture-column.sql
@@ -1,5 +1,4 @@
 -- Add profile picture column to users table
 ALTER TABLE users ADD COLUMN profile_picture VARCHAR(255) DEFAULT NULL;
 
--- Add index on profile_picture for optimization
-CREATE INDEX idx_profile_picture ON users(profile_picture);
+
